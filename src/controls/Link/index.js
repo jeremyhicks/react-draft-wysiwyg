f2418cc4ce19e068153531aba01852c7483f5bcf
Jeremy Hicks <jeremy.hicks@pnmac.com>
2019-08-28 08:47:20 -0700

    Add option to use linkify, defaults to true

--- src/controls/Link/index.js
+++ src/controls/Link/index.js
@@ -65,13 +65,17 @@ class Link extends Component {
 
   onChange = (action, title, target, targetOption) => {
     const {
-      config: { linkCallback }
+      config: { linkCallback, linkify }
     } = this.props;
 
     if (action === "link") {
-      const linkifyCallback = linkCallback || linkifyLink;
-      const linkified = linkifyCallback({ title, target, targetOption });
-      this.addLink(linkified.title, linkified.target, linkified.targetOption);
+      if (linkify) {
+        const linkifyCallback = linkCallback || linkifyLink;
+        const linkified = linkifyCallback({ title, target, targetOption });
+        this.addLink(linkified.title, linkified.target, linkified.targetOption);
+      } else {
+        this.addLink(title, target, targetOption);
+      }
     } else {
       this.removeLink();
     }
